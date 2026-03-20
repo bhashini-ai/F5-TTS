e5280b328ff835de608e865a369104281920e240
Shiva Kumar H R <shivahr@gmail.com>
2026-03-20 21:54:07 +0530

    PEFT Dependencies

--- src/f5_tts/runtime/triton_trtllm/docker/interactive.sh
+++ src/f5_tts/runtime/triton_trtllm/docker/interactive.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it --rm --name "f5-server" --gpus all --net host -v /home/models:/models -v ~/git/F5-TTS/:/workspace/F5-TTS/ --shm-size=2g triton-f5-tts:24.12
+docker run -it --rm --name "f5-server" --gpus '"device=1"' --net host -v /home/models:/models -v ~/git/F5-TTS/:/workspace/F5-TTS/ -v /data3/TTS_Speech_Recording/:/training -v /home/reference-audios:/reference-audios --shm-size=2g triton-f5-tts:24.12
